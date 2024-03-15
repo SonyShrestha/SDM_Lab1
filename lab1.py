13955f8585983554e2ea6c7cdb86646f6cf7e447
sony <sony.sth8@gmail.com>
2024-03-15 15:42:56 +0100

    Python file rearranged

--- lab1.py
+++ lab1.py
@@ -1,5 +1,21 @@
 from neo4j import GraphDatabase
 import pandas as pd
+import argparse
+import json
+import logging
+import configparser
+import get_paper_ids, get_paper_details, preprocess_data, split_files
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)  # Set log level to INFO
+
+# Create logger object
+logger = logging.getLogger()
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
 
 class Neo4jConnector:
     def __init__(self, uri, user, password):
@@ -103,14 +119,54 @@ def recommender(connector):
 
 
 if __name__ == "__main__":
-    # Example usage
-    uri = "bolt://localhost:7687"  # Replace with your Neo4j server URI
-    user = "neo4j"         # Replace with your Neo4j username
-    password = "abcd1234"     # Replace with your Neo4j password
+    parser = argparse.ArgumentParser(description="Implementation of graph database")
+    
+    # Extract config from config.ini file
+    uri = config["NEO4J"]["uri"]
+    user = config["NEO4J"]["user"]
+    password = config["NEO4J"]["password"]
+    api_key = config["API"]["api_key"]
+
+    # Add arguments
+    parser.add_argument("--field", required=True, help="Comma separated fields to search for papers")
+    # Parse arguments
+    args = parser.parse_args()
+    fields = args.field.split(',')
+
+    logger.info("--------------------- FETCHING PAPER IDS ---------------------")
+    # Retrieve paper information
+    for field in fields:
+        paper_info = get_paper_ids.get_paper_info(api_key, field)
+        if paper_info:
+            with open(f'paper_ids/paper_{field}.json', mode='w', encoding='utf-8') as file:
+                json.dump(paper_info, file, indent=4)
+        else:
+            print("Failed to retrieve paper information.")
+
+
+    logger.info("--------------------- FETCHING PAPER DETAILS FROM EXTRACTED PAPER IDS ---------------------")
+    for field in fields:
+        get_paper_details.fetch_publications(field,api_key)
+    
+
+    logger.info("--------------------- PREPROCESSING PAPER DETAILS ---------------------")
+    preprocess_data.preprocess_data()
+    
+
+    logger.info("--------------------- SPLITTING PAPER DETAILS INTO SEPARATE FILES ---------------------")
+    split_files.get_years()
+    split_files.get_authors()
+    split_files.get_keywords()
+    split_files.get_journals()
+    split_files.get_workshops()
+    split_files.get_conferences()
+    split_files.get_conference_papers()
+    split_files.get_journal_papers()
+    split_files.get_workshop_papers()
 
     connector = Neo4jConnector(uri, user, password)
     connector.connect()
-    
+
     top_3_cited_paper(connector)
     conference_community(connector)
     h_index(connector)
